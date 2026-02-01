0de90c26d48a09eea6a00878693c51383f05a716
Ziad Elshihy <elshihyziad@gmail.com>
2026-02-01 22:25:14 +0200

    Add basic Java program to print welcome message

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Akla-7ersha Project 🚀");
+        System.out.println("Java project is running successfully!");
+    }
+}
