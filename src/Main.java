6e1d62797688fc27c5dfbdd63c2ab2627ee61122
Ziad Elshihy <125217591+ziad-elshihy@users.noreply.github.com>
2026-04-06 18:53:52 +0200

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello from Akla-7ersha Project 🚀");
-        System.out.println("Java project is running successfully!");
-    }
-}
